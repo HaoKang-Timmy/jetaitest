0567a73b3e8217e4412e86970c6bc7b9ee5cb083
Hao Kang <hkang342@atl1-1-01-006-19-0.pace.gatech.edu>
2025-10-02 18:47:52 -0400

    test kkt

--- test/server_models/jetinfra/compare_with_fla.py
+++ test/server_models/jetinfra/compare_with_fla.py
@@ -1,258 +1,253 @@
-# Util functions for flash linear attention cumsum
-# Reference: fla/ops/utils/cumsum.py
+# Reference: fla/ops/common/chunk_scaled_dot_kkt.py
 
 import tilelang
 import tilelang.language as T
 import sys  # noqa: F401
+import time
 import itertools
 from tilelang.autotuner import autotune
-
 # Add your fla repository path to sys.path
 # Currently we use the fla repository from the flash-linear-attention project at commit id f03cb3ae
 # sys.path.insert(0, "/home/tzj/flash-linear-attention")
 try:
     import fla
     print(fla.__file__)
-    from fla.ops.utils.cumsum import chunk_local_cumsum_scalar
+    from fla.ops.common.chunk_scaled_dot_kkt import chunk_scaled_dot_kkt_fwd
 except ImportError:
     print("fla not found, using tilelang implementation")
     fla = None
 
 import torch
 
+torch.set_printoptions(profile="full")
+torch.random.manual_seed(0)
+
 tilelang.disable_cache()
 
+
+def prepare_input(
+    B,
+    S,
+    H,
+    DK,
+    input_dtype,
+    output_dtype,
+    accum_dtype,
+):
+    K = torch.randn(B, S, H, DK, dtype=input_dtype).cuda()
+    Beta = torch.randn(B, S, H, dtype=input_dtype).cuda()
+    G = torch.randn(B, S, H, dtype=accum_dtype).cuda()
+    return K, Beta, G
+
+
+def prepare_output(
+    B,
+    S,
+    H,
+    chunk_size,
+    dtype,
+):
+    BS = chunk_size
+    A = torch.empty(B, S, H, BS, dtype=dtype).cuda()
+    return A
+
 def get_configs():
-    Block_T = [64 ,128, 256, 512]
-    threads = [128]
-    _configs = list(itertools.product(Block_T, threads))
+    block_S = [64]
+    block_DK = [64]
+    threads = [128, 256]
+    num_stages = [1, 2, 3, 4]
+    _configs = list(itertools.product(block_S, block_DK, threads, num_stages))
     configs = [{
-        'Block_T': c[0],
-        'threads': c[1]
+        'block_S': c[0],
+        'block_DK': c[1],
+        'threads': c[2],
+        'num_stages': c[3]
     } for c in _configs]
     return configs
-
+# tilelang.disable_cache()
 @autotune(configs=get_configs(), warmup=10, rep=10)
-@tilelang.jit(
-    out_idx=[-1]
-)
-def tl_chunk_cumsum(
-    B,
-    Token,
-    H,
-    chunk_size = 64,
-    input_dtype = "bfloat16",
-    output_dtype = "float32",
-    Block_T = 128,
-    threads = 256,
-):
-    assert Block_T % chunk_size == 0, "Block_T must be divisible by chunk_size"
-    chunk_num = Block_T // chunk_size
-    fragment_shape = [chunk_num, chunk_size]
-    @T.prim_func
-    def kernel(
-        InputG: T.Tensor(shape=(B, Token, H), dtype=input_dtype),
-        OutputG: T.Tensor(shape=(B, Token, H), dtype=output_dtype)
-    ):
-        with T.Kernel(T.ceildiv(Token, Block_T), B * H, threads=threads) as (bt, bbh):
-            bb, bh = bbh // H, bbh % H
-            InputG_shared = T.alloc_shared((Block_T), dtype=input_dtype)
-            InputG_fragment = T.alloc_fragment((Block_T), dtype=output_dtype)
-            T.copy(InputG[bb, bt * Block_T:(bt + 1) * Block_T, bh], InputG_shared)
-
-            T.copy(InputG_shared, InputG_fragment)
-            InputG_fragment_viewed = T.view(InputG_fragment, fragment_shape)
-            T.cumsum(InputG_fragment_viewed, dim=1)
-            InputG_fragment_viewed_reverse = T.view(InputG_fragment_viewed, [Block_T])
-            T.copy(InputG_fragment_viewed_reverse, InputG_shared)
-            T.copy(InputG_shared, OutputG[bb, bt * Block_T:(bt + 1) * Block_T, bh])
-    return kernel
-
-@tilelang.jit(
-    out_idx=[-1],
-    pass_configs={
-        tilelang.PassConfigKey.TL_DISABLE_TMA_LOWER: True,
-        tilelang.PassConfigKey.TL_DISABLE_WARP_SPECIALIZED: True
-    })
-def tilelang_chunk_local_cumsum_scalar(
+@tilelang.jit(out_idx=[-1])
+def tilelang_chunk_scaled_dot_kkt_fwd(
     # task config
     B,
     S,
     H,
+    DK,
     chunk_size=64,
-    is_varlen=False,
-    head_first=False,
-    reverse=False,
-    input_dtype="float16",
-    output_dtype="float32",
+    input_dtype="bfloat16",
+    output_dtype="bfloat16",
+    accum_dtype="float32",
+    use_g=True,
     # kernel config
     block_S=64,
-    threads=256,
-    use_fragment=False,
+    block_DK=64,
+    threads=128,
+    num_stages=2,
 ):
-    G_shape = (B, H, S) if head_first else (B, S, H)
-    assert chunk_size == 2**(chunk_size.bit_length() - 1), "chunk_size must be a power of 2"
+    K_shape = (B, S, H, DK)
+    Beta_shape = (B, S, H)
+    G_shape = (B, S, H)
     assert chunk_size == block_S, "chunk_size must be equal to block_S"
-
+    BS = chunk_size
+    output_shape = (B, S, H, BS)
+    
     @T.prim_func
     def kernel(
-            G: T.Tensor(G_shape, dtype=input_dtype),
-            G_new: T.Tensor(G_shape, dtype=output_dtype),
+            K: T.Tensor(K_shape, dtype=input_dtype),
+            Beta: T.Tensor(Beta_shape, dtype=input_dtype),
+            G: T.Tensor(G_shape, dtype=accum_dtype),
+            A: T.Tensor(output_shape, dtype=output_dtype),
     ):
         with T.Kernel(T.ceildiv(S, block_S), B * H, threads=threads) as (bs, bbh):
             bb, bh = bbh // H, bbh % H
-            G_shared = T.alloc_shared((1, block_S), dtype=output_dtype, scope="shared")
-            if head_first:
-                T.copy(G[bb, bh, bs * block_S:(bs + 1) * block_S], G_shared)
+            # !! Pay attention to the scope of the shared memory: may cause misaligned address when shape is one dimension or the buffer is too small
+            Beta_shared = T.alloc_shared((block_S,), dtype=input_dtype, scope="shared")
+            K_shared = T.alloc_shared((block_S, block_DK), dtype=input_dtype)
+            A_shared = T.alloc_shared((block_S, block_S), dtype=output_dtype)
+            Beta_K_fragment = T.alloc_fragment((block_S, block_DK), dtype=input_dtype)
+            A_fragment = T.alloc_fragment((block_S, block_S), dtype=accum_dtype)
+
+            # Tensor used for gated:
+            G_shared = T.alloc_shared((block_S,), dtype=accum_dtype, scope="shared")
+            G_diff_local = T.alloc_fragment((block_S, block_S), dtype=accum_dtype)
+            # G_frag = T.alloc_fragment((block_S), dtype=accum_dtype)
+
+            T.annotate_layout({
+                K_shared: tilelang.layout.make_swizzled_layout(K_shared),
+                A_shared: tilelang.layout.make_swizzled_layout(A_shared),
+            })
+
+            T.fill(A_fragment, 0)
+            T.disable_warp_group_reg_alloc()
+            for i_s in T.Parallel(block_S):
+                Beta_shared[i_s] = Beta[bb, bs * block_S + i_s, bh]
+
+            for i_k in T.Pipelined(T.ceildiv(DK, block_DK), num_stages=num_stages):
+                T.copy(
+                    K[bb, bs * block_S:(bs + 1) * block_S, bh, i_k * block_DK:(i_k + 1) * block_DK],
+                    K_shared)
+                for i_s, i_k2 in T.Parallel(block_S, block_DK):
+                    Beta_K_fragment[i_s, i_k2] = K_shared[i_s, i_k2] * Beta_shared[i_s]
+                T.gemm(Beta_K_fragment, K_shared, A_fragment, transpose_B=True)
+
+            if use_g:
+                for i_s in T.Parallel(block_S):
+                    G_shared[i_s] = G[bb, bs * block_S + i_s, bh]
+                for i_s1, i_s2 in T.Parallel(block_S, block_S):
+                    G_diff_local[i_s1, i_s2] = G_shared[i_s1] - G_shared[i_s2]
+                for i_s1, i_s2 in T.Parallel(block_S, block_S):
+                    with T.If(G_diff_local[i_s1, i_s2] <= 0 and i_s1 > i_s2):
+                        with T.Then():
+                            A_fragment[i_s1, i_s2] = A_fragment[i_s1, i_s2] * T.exp(
+                                G_diff_local[i_s1, i_s2])
+                        with T.Else():
+                            A_fragment[i_s1, i_s2] = 0
+                # for i_s1, i_s2 in T.Parallel(block_S, block_S):
+                #     with T.If(G_frag[i_s1] <= G_frag[i_s2] and i_s1 > i_s2):
+                #         with T.Then():
+                #             A_fragment[i_s1, i_s2] = A_fragment[i_s1, i_s2] * T.exp(
+                #                 G_frag[i_s1] - G_frag[i_s2])
+                #         with T.Else():
+                #             A_fragment[i_s1, i_s2] = 0
             else:
-                T.copy(G[bb, bs * block_S:(bs + 1) * block_S, bh], G_shared)
-            if use_fragment:
-                G_fragment = T.alloc_fragment((1, block_S), dtype=output_dtype, scope="shared")
-                T.copy(G_shared, G_fragment)
-                T.cumsum(G_fragment, dim=1, reverse=reverse)
-                if head_first:
-                    T.copy(G_fragment, G_new[bb, bh, bs * block_S:(bs + 1) * block_S])
-                else:
-                    T.copy(G_fragment, G_new[bb, bs * block_S:(bs + 1) * block_S, bh])
-            else:
-                T.cumsum(G_shared, dim=1, reverse=reverse)
-                if head_first:
-                    T.copy(G_shared, G_new[bb, bh, bs * block_S:(bs + 1) * block_S])
-                else:
-                    T.copy(G_shared, G_new[bb, bs * block_S:(bs + 1) * block_S, bh])
+                for i_s1, i_s2 in T.Parallel(block_S, block_S):
+                    with T.If(i_s1 <= i_s2):  # noqa: SIM117
+                        with T.Then():
+                            A_fragment[i_s1, i_s2] = 0
+
+            T.copy(A_fragment, A_shared)
+            T.copy(A_shared, A[bb, bs * block_S:(bs + 1) * block_S, bh, :])
 
     return kernel
-
-
-def prepare_cumsum_input(
-    B,
-    S,
-    H,
-    dtype,
-):
-    G = torch.randn(B, S, H, dtype=dtype).cuda()
-    return G
-
-
-def prepare_cumsum_output(
-    B,
-    S,
-    H,
-    dtype,
-):
-    G_new = torch.empty(B, S, H, dtype=dtype).cuda()
-    return G_new
 
 
 def run_test(
     B,
     S,
     H,
+    DK,
     chunk_size,
-    reverse,
-    head_first,
     input_dtype,
     output_dtype,
+    accum_dtype,
+    use_g,
+    block_DK,
     threads,
-    use_fragment,
+    num_stages,
 ):
-    G = prepare_cumsum_input(B, S, H, getattr(torch, input_dtype))
-    G_new_ref = prepare_cumsum_output(B, S, H, getattr(torch, output_dtype))
-    G_new_tilelang = prepare_cumsum_output(B, S, H, getattr(torch, output_dtype))
-
-    # reference cumsum
+    K, Beta, G = prepare_input(B, S, H, DK, getattr(torch, input_dtype),
+                               getattr(torch, output_dtype), getattr(torch, accum_dtype))
+    A_ref = prepare_output(B, S, H, chunk_size, getattr(torch, output_dtype))
+    A_tilelang = prepare_output(B, S, H, chunk_size, getattr(torch, output_dtype))
     for _ in range(10):
-        G_new_ref = chunk_local_cumsum_scalar(
-            g=G,
-            chunk_size=chunk_size,
-            reverse=reverse,
-            head_first=head_first,
-            output_dtype=getattr(torch, output_dtype))
-    import time
+    # reference
+        if use_g:
+            A_ref = chunk_scaled_dot_kkt_fwd(
+                K, Beta, G, chunk_size=chunk_size, output_dtype=getattr(torch, output_dtype))
+        else:
+            A_ref = chunk_scaled_dot_kkt_fwd(
+                K, Beta, None, chunk_size=chunk_size, output_dtype=getattr(torch, output_dtype))
+
+
     torch.cuda.synchronize()
     start_time = time.time()
     for _ in range(10):
-        G_new_ref = chunk_local_cumsum_scalar(
-            g=G,
-            chunk_size=chunk_size,
-            reverse=reverse,
-            head_first=head_first,
-            output_dtype=getattr(torch, output_dtype))
+        if use_g:
+            A_ref = chunk_scaled_dot_kkt_fwd(
+                K, Beta, G, chunk_size=chunk_size, output_dtype=getattr(torch, output_dtype))
+        else:
+            A_ref = chunk_scaled_dot_kkt_fwd(
+                K, Beta, None, chunk_size=chunk_size, output_dtype=getattr(torch, output_dtype))
     torch.cuda.synchronize()
     end_time = time.time()
-    print(f"chunk_local_cumsum_scalar time: {end_time - start_time}")
-    kernel = tl_chunk_cumsum(
-            B=B,
-            Token=S,
-            H=H,
-            chunk_size=chunk_size,
-            input_dtype=input_dtype,
-            output_dtype=output_dtype,
-        )
+    print(f"triton Time taken: {end_time - start_time} seconds")
+
+    # tilelang
+    block_S = chunk_size
     for _ in range(10):
-        G_new_tilelang = kernel(G)
+        # kernel = tilelang_chunk_scaled_dot_kkt_fwd(B, S, H, DK, chunk_size, input_dtype, output_dtype,
+        #                                         accum_dtype, use_g, block_S, block_DK, threads,
+        #                                         num_stages)
+        kernel = tilelang_chunk_scaled_dot_kkt_fwd(B, S, H, DK, chunk_size, input_dtype, output_dtype,
+                                            accum_dtype, use_g)
+        A_tilelang = kernel(K, Beta, G)
     torch.cuda.synchronize()
     start_time = time.time()
     for _ in range(10):
-        G_new_tilelang = kernel(G)
+        # kernel = tilelang_chunk_scaled_dot_kkt_fwd(B, S, H, DK, chunk_size, input_dtype, output_dtype,
+        #                                         accum_dtype, use_g, block_S, block_DK, threads,
+        #                                         num_stages)
+        kernel = tilelang_chunk_scaled_dot_kkt_fwd(B, S, H, DK, chunk_size, input_dtype, output_dtype,
+                                            accum_dtype, use_g)
+        A_tilelang = kernel(K, Beta, G)
     torch.cuda.synchronize()
     end_time = time.time()
-    print(f"tl_chunk_cumsum time: {end_time - start_time}")
-
-
-
-    # # tilelang cumsum
-    # block_S = chunk_size
-    # kernel = tilelang_chunk_local_cumsum_scalar(
-    #     B=B,
-    #     S=S,
-    #     H=H,
-    #     chunk_size=chunk_size,
-    #     reverse=reverse,
-    #     head_first=head_first,
-    #     input_dtype=input_dtype,
-    #     output_dtype=output_dtype,
-    #     block_S=block_S,
-    #     threads=threads,
-    #     use_fragment=use_fragment,
-    # )
-    # for _ in range(10):
-    #     G_new_tilelang = kernel(G)
-    # import time
-    # torch.cuda.synchronize()
-    # start_time = time.time()
-    # for _ in range(10):
-    #     G_new_tilelang = kernel(G)
-    # torch.cuda.synchronize()
-    # end_time = time.time()
-    print(f"tilelang_chunk_local_cumsum_scalar time: {end_time - start_time}")
+    print(f"tilelang Time taken: {end_time - start_time} seconds")
 
     try:
-        torch.testing.assert_close(G_new_tilelang, G_new_ref, rtol=1e-2, atol=1e-2)
-        print("tilelang cumsum passed √")
+        torch.testing.assert_close(A_tilelang, A_ref, rtol=1e-2, atol=1e-2)
+        print("tilelang chunk scaled dot kkt fwd passed √")
     except Exception as e:
-        print("tilelang cumsum failed ✗")
+        print("tilelang chunk scaled dot kkt fwd failed ✗")
         print(e)
-        print("G:")
-        print(G.view(-1))
-        print("G_new_tilelang:")
-        print(G_new_tilelang.view(-1))
-        print("G_new_ref:")
-        print(G_new_ref.view(-1))
+        print("reference cuda kernel:")
+        print(kernel.get_kernel_source())
 
 
 def main():
     run_test(
         B=1,
         S=32768,
-        H=12,
+        H=32,
+        DK=128,
         chunk_size=64,
-        reverse=False,
-        head_first=False,
-        input_dtype="float32",
-        output_dtype="float32",
-        threads=256,
-        use_fragment=False)
+        input_dtype="bfloat16",
+        output_dtype="bfloat16",
+        accum_dtype="float32",
+        use_g=True,
+        block_DK=64,
+        threads=128,
+        num_stages=2)
 
 
 if __name__ == "__main__":
